9b0316260062bbfa0ff3a5f2dadef518d1b43fd1
Frolov Danil <frolovdanil75@gmail.com>
2021-09-22 23:15:07 +0300

    add button for create new item

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -9,13 +9,15 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["Write letter", "Watch lesson", "Complete todoe app"]
+    var itemArray = ["Write letter", "Watch lesson", "Complete todoe app"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
+    // MARK: - Tableview Datasource Methods
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -27,6 +29,42 @@ class TodoListViewController: UITableViewController {
         
         return cell
     }
-
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    // MARK: - Add new item
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textfield = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { UIAlertAction in
+            if textfield.text! != "" {
+                self.itemArray.append(textfield.text!)
+                self.tableView.reloadData()
+            } else {
+                self.itemArray.append("New Item")
+                self.tableView.reloadData()
+            }
+        }
+        
+        alert.addTextField { allertTextField in
+            allertTextField.placeholder = "Create new item"
+            textfield = allertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
